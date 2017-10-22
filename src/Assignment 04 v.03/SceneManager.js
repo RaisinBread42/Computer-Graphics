3cb979b07858ca55f37d6821c31f6e891567e7bf
SkyyCipp <williamsshannonj@gmail.com>
2017-10-21 20:17:27 -0400

    Added Texture Loading
    Render loaded .OBJ after finished async loading of image on start up.
    
    Possible memory leak?

---
+++ src/Assignment 04 v.03/SceneManager.js
@@ -0,0 +1,71 @@
+
+var SceneManager = {
+
+/* List of all objects in the scene*/
+
+Objects: new Array(),
+SelectedObjects : new Array(),
+
+ObjectsBuffer: new Array(),
+SelectedObjectsBuffer: new Array(),
+
+/* Get/Set variables */
+
+AmbientLight: new Array(),
+DirectionLightPosition: new Array(),
+DirectionLightColor: new Array(),
+Image: new Image(),
+
+SetDirLightPosition: function(arr){
+this.DirectionLightPosition = arr;
+},
+
+InitializeVariables: function(){
+
+  ControlManager.ctx = document.getElementById("xlocalpos").value;
+  ControlManager.cty = document.getElementById("ylocalpos").value;
+  ControlManager.ctz = document.getElementById("zlocalpos").value;
+  ControlManager.csx = document.getElementById("xlocalscale").value;
+  ControlManager.csy = document.getElementById("ylocalscale").value;
+  ControlManager.csz = document.getElementById("zlocalscale").value;
+  ControlManager.cxrot = document.getElementById("xlocalrotation").value;
+  ControlManager.cyrot = document.getElementById("ylocalrotation").value;
+  ControlManager.czrot = document.getElementById("zlocalrotation").value;
+
+var i;
+while( i=0 < this.SelectedObjects.length){
+
+	var o=SelectedObjects[i];
+
+  	o.tx = ControlManager.ctx; o.ty = ControlManager.cty; o.tz = ControlManager.ctz;
+  	o.sx = ControlManager.csx; o.sy = ControlManager.csy; o.sz = ControlManager.csz;
+  	o.xrot = ControlManager.cxrot; o.yrot = ControlManager.cyrot; o.zrot = ControlManager.czrot;
+}
+
+},
+
+LoadObject: function(verts, vindexes, vnormals, textcoords){
+
+var o = {
+vertices : verts,
+verticeIndexes: vindexes,
+verticenormals: vnormals,
+textureCoords: textcoords,
+isActive : true,
+isSelected : true
+}
+this.Objects.push(o);
+this.SelectedObjects.push(o);
+this.SelectedObjectsBuffer.push(o);
+this.ObjectsBuffer.push(o);
+
+},
+
+ClearBuffers: function(){
+
+SelectedObjectsBuffer = new Array();
+ObjectsBuffer = new Array();
+}
+
+
+}; // End of SceneManager Object
---
+++ src/Assignment 04 v.03/main.js
@@ -0,0 +1,160 @@
+
+let fudgingfactor = document.getElementById("perspectiveviewvalue").value;
+
+
+function InitializeWindow(gl){
+
+	// Clear the canvas.
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+
+    let shaderprogram = initShaderProgram(gl,vsSource,fsSource);
+    gl.useProgram(shaderprogram);
+
+    let image = new Image();
+    image.src="http://localhost:8080/tile.jpeg";
+    image.onload = function(){
+    console.log(image);
+    SceneManager.Image = image;
+    render(gl, shaderprogram);
+    }
+
+    SceneManager.InitializeVariables();
+
+}
+
+function MainLoop(gl, shaderprogram){
+
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+    let positionLocation = gl.getAttribLocation(shaderprogram, "a_position");
+    let vertnormalLocation = gl.getAttribLocation(shaderprogram,"v_normal");
+    let ambientLocation = gl.getUniformLocation(shaderprogram,"ambientLight");
+    let directionalVectorLocation = gl.getUniformLocation(shaderprogram,"directionalVector");
+  	let matrixLocation = gl.getUniformLocation(shaderprogram, "u_matrix");
+    let normalmatrixLocation = gl.getUniformLocation(shaderprogram,"uNormalMatrix");
+    let textureUVLocation = gl.getAttribLocation(shaderprogram, "a_texCoord");
+  	
+    //perspective to be done in shader
+    let fudgefactorlocation = gl.getUniformLocation(shaderprogram, "fudgefactor");
+    gl.uniform1f(fudgefactorlocation,fudgingfactor);
+
+    gl.uniform3fv(ambientLocation, SceneManager.AmbientLight);
+    gl.uniform3fv(directionalVectorLocation, SceneManager.DirectionLightPosition);
+
+    let el = 0 ;
+    while( el < SceneManager.SelectedObjectsBuffer.length){
+
+    // bind buffers and pass info to shader program for computing lighting
+    let normalmatrix = TransformNormalMatrix();
+    BufferVertexNormal(gl, el, vertnormalLocation);
+    gl.uniformMatrix4fv(normalmatrixLocation, false, normalmatrix); // set the normalmatrixLocation let to normalmatrix
+
+    // set up texture rendering
+
+    BufferTexture(gl, SceneManager.Image, SceneManager.SelectedObjectsBuffer[el].textureCoords, textureUVLocation);
+
+    // compute transformations
+    let matrix = TransformModelMatrix();
+    BufferVertexPosition(gl, el, positionLocation); // bind vertex position buffer
+    
+    gl.uniformMatrix4fv(matrixLocation, false, matrix); // set the matrixLocation let to matrix
+
+    // Draw the geometry.
+    let primitiveType = gl.TRIANGLES;
+    let offset = 0;
+    let count = (SceneManager.SelectedObjectsBuffer[el].verticeIndexes.length > 0)  ? SceneManager.SelectedObjectsBuffer[el].verticeIndexes.length: 0;  
+    gl.drawArrays(primitiveType, offset, count);
+
+    el++;
+}
+    gl.bindBuffer(gl.ARRAY_BUFFER, null); // unbinding
+}
+
+function BufferTexture(gl, image ,textcoords, texCoordLocation){
+  var texCoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textcoords), gl.STATIC_DRAW);
+  gl.enableVertexAttribArray(texCoordLocation);
+  gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
+ 
+  // Create a texture.
+  var texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+ 
+  // Set the parameters so we can render any size image.
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+ 
+  // Upload the image into the texture.
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+}
+
+function BufferVertexNormal(gl, elindex, vertnormalLocation){
+    let vertnormalbuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertnormalbuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(SceneManager.SelectedObjectsBuffer[elindex].verticenormals), gl.STATIC_DRAW);
+
+    // Turn on the attribute to pull vertices from the currently binded vertex array buffer
+    gl.enableVertexAttribArray(vertnormalLocation);
+
+    // Tell the attribute how to get data out of vertexnormalBuffer (ARRAY_BUFFER)
+    let size = 3;          // 2 components per iteration
+    let type = gl.FLOAT;   // the data is 32bit floats
+    let normalize = false; // don't normalize the data
+    let stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    let offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        vertnormalLocation, size, type, normalize, stride, offset)
+}
+
+function BufferVertexPosition(gl,elindex,shaderverposition){
+    // Create a buffer to put positions in
+    let positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(SceneManager.SelectedObjectsBuffer[elindex].vertices), gl.STATIC_DRAW);
+
+    // Turn on the attribute to pull vertices from the currently binded vertex array buffer
+    gl.enableVertexAttribArray(shaderverposition);
+
+    // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    let size = 3;          // 2 components per iteration
+    let type = gl.FLOAT;   // the data is 32bit floats
+    let normalize = false; // don't normalize the data
+    let stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    let offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        shaderverposition, size, type, normalize, stride, offset)
+
+}
+
+function TransformNormalMatrix(){
+
+    let matrix = m4.identity();
+    let scalematrix = m4.scaling(ControlManager.csx, ControlManager.csy, ControlManager.csz);
+    let rotationmatrix = m4.rotate(matrix, ControlManager.cxrot, ControlManager.cyrot, ControlManager.czrot);
+
+    matrix = m4.VertexNormalInverseTranspose(matrix, scalematrix, rotationmatrix);
+    return matrix;
+}
+
+function TransformModelMatrix(){
+
+    let matrix = m4.identity();
+
+    matrix = m4.rotate(matrix, ControlManager.cxrot, ControlManager.cyrot, ControlManager.czrot);
+    matrix = m4.scale(matrix, ControlManager.csx, ControlManager.csy, ControlManager.csz);
+    matrix = m4.translate(matrix, ControlManager.ctx, ControlManager.cty, ControlManager.ctz);
+
+    return matrix;
+}
+
+
+function render(gl, shaderprogram){
+  MainLoop(gl,shaderprogram);
+  window.setTimeout(function() {render(gl,shaderprogram);}, 1000/60);
+}
+
